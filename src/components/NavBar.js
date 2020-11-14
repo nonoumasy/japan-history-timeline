dddad4f2592eb4d635d6737b5b420123c94d58aa
nonoumasy <nonoumasy@gmail.com>
2020-11-13 19:34:28 -0800

    changed to iconbutton

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,8 +5,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography'
-import { Button } from '@material-ui/core';
 import Link from '@material-ui/core/Link';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -39,14 +41,14 @@ const NavBar = (props) =>  {
                             href="/"
                             style={{ textDecoration: 'none', color: '#fff' }}
                         >
-                            Japan History Timeline
+                            Timelines: <span>Japan History </span>
                         </Link>
                     </Typography>
-                    <Button variant="contained" color='secondary' onClick={handleMenuClick}>
-                        <Typography className={classes.event}>
-                            New Event
-                        </Typography> 
-                    </Button>
+                    <Tooltip title="Add New Event">
+                        <IconButton color='secondary' onClick={handleMenuClick}>
+                            <AddCircleIcon fontSize='large' />
+                        </IconButton>
+                    </Tooltip>
                 </Toolbar>
             </AppBar>
         </div>
