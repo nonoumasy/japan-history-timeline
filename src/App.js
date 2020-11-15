545013b474efa5344539085485ac5b1d05fe0160
nonoumasy <nonoumasy@gmail.com>
2020-11-14 21:52:25 -0800

    fasfs

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ function App() {
           render={props => <EditEvent {...props} />}
         />
         <Route exact path='/'>
-          <div style={{ marginTop: 80, marginLeft: -210 }}>
+          <div style={{ marginTop: 80, marginLeft: -200 }}>
             <Home addToRefs={addToRefs} />
           </div>
         </Route>
