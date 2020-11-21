533c45105a1509e8a0b4fe3dd7500096fc141200
nonoumasy <nonoumasy@gmail.com>
2020-11-20 17:43:39 -0800

    card styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,13 @@ import { Link } from 'react-router-dom'
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from "@material-ui/core";
+import Typography from '@material-ui/core/Typography'
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import Tooltip from '@material-ui/core/Tooltip';
+import Avatar from '@material-ui/core/Avatar';
+import Dialog from '@material-ui/core/Dialog';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -18,6 +25,37 @@ const useStyles = makeStyles((theme) => ({
         cursor: 'pointer',
         color: '#333'
     },
+    card: {
+        objectFit: 'cover',
+    },
+    gridContainer: {
+        margin: 0,
+        padding: 0,
+    },
+    container: {
+        margin: 0,
+        padding: 0,
+        border: 0,
+        background: 'transparent',
+        cursor: 'pointer',
+        textDecoration: 'none',
+        overflow: 'hidden',
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 6px 20px 0 rgba(0, 0, 0, 0.1)',
+
+    },
+    image: {
+        width: '100%',
+        height: '20rem',
+        objectFit: 'cover',
+        margin: 0,
+        padding: 0,
+        borderRadius: '5px',
+        transition: '0.4s',
+        '&:hover': {
+            transformOrigin: '50% 50%',
+            transform: 'scale(1.1)',
+        },
+    },
 }));
 
 const Home = () => {
@@ -26,12 +64,16 @@ const Home = () => {
 
     fetch('https://japan-history-timeline-api.herokuapp.com/timeline')
     .then(res => res.json())
-        .then(data => setData(data))
+    .then(data => setData(data))
     .catch(err => console.log(err))
+
+    const clickImageHandler = () => {
+
+    }
 
 
     return (
-        <Container component="main" maxWidth="xs">
+        <Container component="main" maxWidth="md">
             <div style={{margins:'20px'}}>
                 Welcome to Timelines.
             </div>
@@ -42,12 +84,32 @@ const Home = () => {
                 </Link>
             </Button>
 
-            {data.map(item => (
-                <>
-                    <h2>{item.timelineTitle}</h2>
-                    <img src={item.timelineImageUrl} alt=""/>
-                </>
-            ))}
+            <Grid
+                container
+                spacing={2}
+                className={classes.gridContainer}
+                justify='start'>
+
+                {data.map(item => {
+                    return (
+
+                        <Grid item xs={12} sm={6} md={4}>
+                            <Tooltip title={item.timelineTitle} arrow placement="top">
+                                <Card className={classes.container}>
+                                    <CardMedia
+                                        component='img'
+                                        className={classes.image}
+                                        image={item.timelineImageUrl}
+                                        onClick={() => clickImageHandler(item.timelineImageUrl)}
+                                    />
+                                </Card>
+                            </Tooltip>
+                        </Grid>
+                    )
+                })
+                }
+            </Grid>
+
             
             
             
