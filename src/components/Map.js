88287052a438a6dfb297d6f01dff67f20212067e
nonoumasy <nonoumasy@gmail.com>
2020-11-25 05:42:49 -0800

    asd

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,12 @@
 import React, {useState} from 'react'
-import ReactMapGL, { Marker, NavigationControl, FullscreenControl} from 'react-map-gl';
+import ReactMapGL, { Marker, NavigationControl, FullscreenControl, WebMercatorViewport} from 'react-map-gl';
 
 import {makeStyles} from '@material-ui/styles'
 
 const useStyles = makeStyles((theme) => ({
     marker: {
         width: 'auto',
-        height: '60px',
+        height: '50px',
         objectFit: 'cover',
         borderRadius: '5px',
         boxShadow: '0 10px 20px 0 rgba(0, 0, 0, 0.5)',
@@ -14,6 +14,29 @@ const useStyles = makeStyles((theme) => ({
 }))
 
 const Map = (props) => {
+
+    const applyToArray = (func, array) => func.apply(Math, array) 
+    const getBounds = (props) => {
+            
+        // Calculate corner values of bounds
+        const eventLong = props.props.event && props.props.event.map(item => item.eventLongitude && item.eventLongitude)
+        const eventLat = props.props.event && props.props.event.map(item => item.eventLatitude && item.eventLatitude)
+        const cornersLongLat = [
+            [applyToArray(Math.min, eventLong), applyToArray(Math.min, eventLat)],
+            [applyToArray(Math.max, eventLong), applyToArray(Math.max, eventLat)]
+        ]
+        
+        console.log(cornersLongLat[0], cornersLongLat[1])
+        // Use WebMercatorViewport to get center longitude/latitude and zoom
+        const viewport = new WebMercatorViewport({ width: 1440, height: 900 })
+            .fitBounds([[12.3, 31.7683], [35.2137, 43]], { padding: 100 } ) 
+
+        const { longitude, latitude, zoom } = viewport
+        return { longitude, latitude, zoom }
+    }
+
+    const bounds = getBounds(props)
+
     const [viewport, setViewport] = useState({
         latitude: 40.5625,
         longitude: 15.4067,
@@ -21,11 +44,11 @@ const Map = (props) => {
         bearing: 0,
         pitch: 30,
         width: "100vw",
-        height: "100vh"
+        height: "100vh",
+        ...bounds
     })
-    const classes = useStyles()
 
-    // console.log(props)
+    const classes = useStyles()
 
     return (
         <>
@@ -40,7 +63,8 @@ const Map = (props) => {
                     position: 'absolute',
                     top: 10,
                     left: 0,
-                    padding: '10px'
+                    padding: '10px',
+                    zIndex: 200
                 }}>
                     <FullscreenControl />
                 </div>
@@ -49,12 +73,14 @@ const Map = (props) => {
                     position: 'absolute',
                     top: 46,
                     left: 0,
-                    padding: '10px'
+                    padding: '10px',
+                    zIndex: 200
                 }}>
                     <NavigationControl />
                 </div>
 
                 {props.props.event && props.props.event.map(item => (
+                    item.eventLatitude && item.eventLongitude &&
                     <Marker latitude={item.eventLatitude} longitude={item.eventLongitude}>
                     <img src={item.eventImageUrl} alt='' className={classes.marker} /> 
                     </Marker>
