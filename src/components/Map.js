25146211e0e9d1aa27ebf6d292e2f43f639bd56b
nonoumasy <nonoumasy@gmail.com>
2020-11-26 21:26:19 -0800

    marker size based on zoom level

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,25 +4,6 @@ import ReactMapGL, { Marker, NavigationControl, FullscreenControl, WebMercatorVi
 import {makeStyles} from '@material-ui/styles'
 
 const useStyles = makeStyles((theme) => ({
-    marker: {
-        width: 'auto',
-        height: '60px',
-        objectFit: 'cover',
-        borderRadius: '5px',
-        boxShadow: '0 10px 20px 0 rgba(0, 0, 0, 0.5)',
-        margin: '0 auto'
-    },
-    markerVideo: {
-        width: '100px',
-        height: '60px',
-        objectFit: 'cover',
-        borderRadius: '5px',
-        boxShadow: '0 10px 20px 0 rgba(0, 0, 0, 0.5)',
-        margin: '0 auto',
-        outline: 0,
-        border: 0,
-        padding: 0,
-    },
     button: {
         border: 'none',
         backgroundColor: 'transparent',
@@ -45,21 +26,19 @@ const Map = (props) => {
 
         // Use WebMercatorViewport to get center longitude/latitude and zoom
         const viewport = new WebMercatorViewport({ width: 800, height: 600 })
-            .fitBounds([[12.3, 31.7683], [35.2137, 43]], { padding: 100 }) 
+            .fitBounds([[12.3, 31.7683], [35.2137, 43]], { padding: 100 })
             // .fitBounds(cornersLongLat, { padding: 100 })
 
         const { longitude, latitude, zoom } = viewport
         return { longitude, latitude, zoom }
-        } 
+    } 
 
     const bounds = getBounds()
     const classes = useStyles()
-
     const [popup, setPopup] = useState(null)
-    // const [showUserPopup, setShowUserPopup] = useState({})
     const [viewport, setViewport] = useState({
-        latitude: 36.2048,
-        longitude: 138.2529,
+        latitude: 0,
+        longitude: 0,
         zoom: 5.5,
         bearing: 0,
         pitch: 30,
@@ -68,7 +47,6 @@ const Map = (props) => {
         ...bounds
     })
     
-
     useEffect(() => {
         const listener = e => {
             if (e.key === 'Escape') {
@@ -84,18 +62,26 @@ const Map = (props) => {
 
     const flyTo = (item) => {
         setViewport({
+            ...viewport,
             longitude: item.eventLongitude,
             latitude: item.eventLatitude,
             zoom: 11,
-            transitionInterpolator: new FlyToInterpolator({ speed: 1.2 }),
+            transitionInterpolator: new FlyToInterpolator({ speed: 1.6 }),
             transitionDuration: 'auto'
         });
     };
+
+    const onClickMarker = (e, item) => {
+        e.preventDefault()
+        setPopup(item)
+        flyTo(item)
+    }
 
     return (
         <>
             <ReactMapGL
                 {...viewport}
+                attributionControl='true'
                 mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
                 // mapStyle="mapbox://styles/mapbox/light-v9"
                 mapStyle="mapbox://styles/mapbox/outdoors-v11"
@@ -123,53 +109,57 @@ const Map = (props) => {
 
                 {props.props.event && props.props.event.map(item => (
                     <>
+                        {/* check to see if there location data */}
+                            {item.eventLatitude && item.eventLongitude &&
+                        <Marker 
+                            latitude={item.eventLatitude} 
+                            longitude={item.eventLongitude}>
 
-                    {/* check to see if there location data */}
-                        {item.eventLatitude && item.eventLongitude &&
-                    <Marker 
-                        latitude={item.eventLatitude} 
-                        longitude={item.eventLongitude}>
+                                <div 
+                                    className={classes.button}
+                                    onClick={(e) => onClickMarker(e, item)}>
 
-                            <div 
-                                className={classes.button}
-                                onClick={e => {
-                                    e.preventDefault()
-                                    setPopup(item)
-                                    flyTo(item)
-                                }}>
-
-                                <div >
-                                    {item.eventImageUrl && item.eventImageUrl.includes('youtube.com') ?
-                                        <iframe
-                                            // component='video'
-                                            // controls
-                                            className={classes.markerVideo}
-                                            src={item.eventImageUrl}
-                                            allowFullScreen
-                                            mozallowfullscreen="mozallowfullscreen"
-                                            msallowfullscreen="msallowfullscreen"
-                                            oallowfullscreen="oallowfullscreen"
-                                            webkitallowfullscreen="webkitallowfullscreen"
-                                            // width='140'
-                                            // height='100'
-                                            allow="accelerometer"
-                                            title={item.year}
-                                        // type="*"
-                                        ></iframe>
-                                        :
-                                        item.eventImageUrl &&
-                                        <div>
-                                            <img src={item.eventImageUrl} alt='' className={classes.marker} /> 
-                                        </div>
-                                    }
+                                    <div >
+                                        {item.eventImageUrl && item.eventImageUrl.includes('youtube.com') ?
+                                            <iframe
+                                                // component='video'
+                                                style={{
+                                                    width: `${viewport.zoom * 1.4}rem`,
+                                                    height: `${viewport.zoom * 1}rem`,
+                                                    objectFit: 'cover',
+                                                    borderRadius: '5px',
+                                                    boxShadow: '0 10px 20px 0 rgba(0, 0, 0, 0.5)',
+                                                    margin: '0 auto',
+                                                    outline: 0,
+                                                    border: 0,
+                                                    padding: 0
+                                                    }}
+                                                src={item.eventImageUrl}
+                                                allowFullScreen
+                                                mozallowfullscreen="mozallowfullscreen"
+                                                msallowfullscreen="msallowfullscreen"
+                                                oallowfullscreen="oallowfullscreen"
+                                                webkitallowfullscreen="webkitallowfullscreen"
+                                                allow="accelerometer"
+                                                title={item.year}
+                                            ></iframe>
+                                            :
+                                            item.eventImageUrl &&
+                                            <div>
+                                                <img src={item.eventImageUrl} alt='' 
+                                                style={{
+                                                    width: 'auto',
+                                                    height:`${viewport.zoom * 1}rem`,
+                                                    objectFit: 'cover',
+                                                    borderRadius: '5px',
+                                                    boxShadow: '0 10px 20px 0 rgba(0, 0, 0, 0.5)',
+                                                    margin: '0 auto'
+                                                }} /> 
+                                            </div>
+                                        }
+                                    </div>
                                 </div>
-                            </div>
-
-                            
-                                    
-                                
-                            
-                    </Marker>}
+                        </Marker>}
 
                         {popup ? (
                             <Popup
@@ -192,14 +182,8 @@ const Map = (props) => {
                         : null}  
                     </>
                     ))
-                    
                 }
-
-                {}
-                
             </ReactMapGL>
-
-            
         </>
     )
 }
