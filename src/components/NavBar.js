85fbb8a24bcab038d21cfc0aa6d5b9ebaf92b34c
nonoumasy <nonoumasy@gmail.com>
2020-11-30 06:34:14 -0800

    asdf

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -29,7 +29,7 @@ const NavBar = () =>  {
                             to="/"
                             style={{ textDecoration: 'none', color: '#fff' }}
                         >
-                            StoryMaps | StoryLines
+                            StoryMaps  |  StoryLines
                         </Link>
                     </Typography>
 
