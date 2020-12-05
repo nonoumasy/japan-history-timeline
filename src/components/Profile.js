fe7e0b3af2bf4f116f28b8a3f7d75758dbd179ac
nonoumasy <nonoumasy@gmail.com>
2020-12-04 21:12:07 -0800

    fasdf

---
+++ src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Profile = () => {
+    return (
+        <>
+            <div style={{ margin: '80px auto', textAlign: 'center' }}>
+                This is your profile, mr.
+            </div>
+            
+        </>
+    )
+}
+
+export default Profile
